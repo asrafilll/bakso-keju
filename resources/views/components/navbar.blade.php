189051372afbdc237a4aaac980e0e9f69f939630
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-09-07 20:29:20 +0700

    add adminlte template

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,91 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a
+                class="nav-link"
+                data-widget="pushmenu"
+                href="#"
+                role="button"
+            ><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item dropdown">
+            <a
+                class="nav-link"
+                data-toggle="dropdown"
+                href="#"
+            >
+                <i class="far fa-bell"></i>
+                <span class="badge badge-warning navbar-badge">15</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">15 Notifications</span>
+                <div class="dropdown-divider"></div>
+                <a
+                    href="#"
+                    class="dropdown-item"
+                >
+                    <span>4 new messages</span>
+                    <span class="float-right text-muted text-sm">3 mins</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a
+                    href="#"
+                    class="dropdown-item"
+                >
+                    <span>8 friend requests</span>
+                    <span class="float-right text-muted text-sm">12 hours</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a
+                    href="#"
+                    class="dropdown-item"
+                >
+                    <span>3 new reports</span>
+                    <span class="float-right text-muted text-sm">2 days</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a
+                    href="#"
+                    class="dropdown-item dropdown-footer"
+                >See All Notifications</a>
+            </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a
+                class="nav-link"
+                data-toggle="dropdown"
+                href="#"
+            >
+                <img
+                    src="https://via.placeholder.com/50"
+                    class="img-circle"
+                    alt="User"
+                    height="100%"
+                />
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <span class="dropdown-item">Admin</span>
+                <div class="dropdown-divider"></div>
+                <a
+                    href="#"
+                    class="dropdown-item"
+                >Your profile</a>
+                <a
+                    href="#"
+                    class="dropdown-item"
+                >Change Password</a>
+                <div class="dropdown-divider"></div>
+                <a
+                    href="#"
+                    class="dropdown-item"
+                >Sign out</a>
+            </div>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,40 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- Brand Logo -->
+    <a
+        href="index3.html"
+        class="brand-link"
+    >
+        <img
+            src="https://via.placeholder.com/60"
+            alt="AdminLTE Logo"
+            class="brand-image img-circle"
+            style="opacity: .8"
+        >
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul
+                class="nav nav-pills nav-sidebar flex-column nav-legacy"
+                data-widget="treeview"
+                role="menu"
+                data-accordion="false"
+            >
+                <x-nav-item :href="url('/')" activeHref="/">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </x-nav-item>
+                <x-nav-item :href="url('/dashboard')" activeHref="dashboard/*">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard 2</p>
+                </x-nav-item>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
