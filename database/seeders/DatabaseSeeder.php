8a41576cd0ca70e9b17f65d8db25257b6e9d56a0
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-09-18 00:59:39 +0700

    feat(seeder): add default user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::firstOrCreate([
+            'email' => 'superadmin@example.com',
+        ], [
+            'name' => 'Super Admin',
+            'password' => 'secret',
+        ]);
     }
 }
