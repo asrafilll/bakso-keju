b88c9eb8c88f663142558eebb574c2f56f68feed
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-09-21 05:14:58 +0700

    fear(inventory): add filter

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\SearchBranchesAction;
 use App\Http\Requests\InventoryStoreRequest;
 use App\Models\Branch;
 use App\Models\Inventory;
@@ -16,57 +17,85 @@ class InventoryController extends Controller
 {
     /**
      * @param Request $request
+     * @param SearchBranchesAction $searchBranchesAction
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
-    {
-        $inventoryQuery = Inventory::query()
-            ->select([
-                'inventories.*',
-                'products.name as product_name',
-                'branches.name as branch_name',
-                'users.name as created_by_name'
-            ])
-            ->join('products', 'inventories.product_id', 'products.id')
-            ->join('branches', 'inventories.branch_id', 'branches.id')
-            ->join('users', 'inventories.created_by', 'users.id');
+    public function index(
+        Request $request,
+        SearchBranchesAction $searchBranchesAction
+    ) {
+        $actions = [
+            'fetch-branches' => function () use ($request, $searchBranchesAction) {
+                return Response::json(
+                    $searchBranchesAction->execute(
+                        $request->get('term')
+                    )
+                );
+            },
+            'default' => function () use ($request) {
+                $inventoryQuery = Inventory::query()
+                    ->select([
+                        'inventories.*',
+                        'products.name as product_name',
+                        'branches.name as branch_name',
+                        'users.name as created_by_name'
+                    ])
+                    ->join('products', 'inventories.product_id', 'products.id')
+                    ->join('branches', 'inventories.branch_id', 'branches.id')
+                    ->join('users', 'inventories.created_by', 'users.id');
 
-        if ($request->filled('filter')) {
-            $inventoryQuery->where(function ($query) use ($request) {
+                if ($request->filled('term')) {
+                    $inventoryQuery->where(function ($query) use ($request) {
+                        $searchables = [
+                            'products.name',
+                            'branches.name',
+                            'users.name',
+                            'inventories.quantity',
+                        ];
+
+                        foreach ($searchables as $searchable) {
+                            $query->orWhere($searchable, 'LIKE', "%{$request->get('term')}%");
+                        }
+                    });
+                }
+
                 $filterables = [
-                    'products.name',
-                    'branches.name',
-                    'users.name',
-                    'inventories.quantity',
+                    'branch_id',
                 ];
 
                 foreach ($filterables as $filterable) {
-                    $query->orWhere($filterable, 'LIKE', "%{$request->get('filter')}%");
+                    if ($request->filled($filterable)) {
+                        $inventoryQuery->where($filterable, $request->get($filterable));
+                    }
                 }
-            });
-        }
 
-        $inventories = $inventoryQuery->latest()->paginate();
+                $inventories = $inventoryQuery->latest()->paginate();
 
-        return Response::view('inventory.index', [
-            'inventories' => $inventories,
-        ]);
+                return Response::view('inventory.index', [
+                    'inventories' => $inventories,
+                ]);
+            },
+        ];
+
+        return $actions[$request->get('action', 'default')]();
     }
 
     /**
      * @param Request
+     * @param SearchBranchesAction $searchBranchesAction
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request)
-    {
+    public function create(
+        Request $request,
+        SearchBranchesAction $searchBranchesAction
+    ) {
         $actions = [
-            'fetch-branches' => function () use ($request) {
-                $branches = Branch::query()
-                    ->where('name', 'LIKE', "%{$request->get('term')}%")
-                    ->orderBy('name')
-                    ->get();
-
-                return Response::json($branches);
+            'fetch-branches' => function () use ($request, $searchBranchesAction) {
+                return Response::json(
+                    $searchBranchesAction->execute(
+                        $request->get('term')
+                    )
+                );
             },
             'fetch-products' => function () use ($request) {
                 $products = Product::query()
