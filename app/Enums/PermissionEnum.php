b9e71e2c96d056d3c3c909ac1a467437018df962
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-09-27 21:00:05 +0700

    feat(permission): PermissionEnum

---
+++ app/Enums/PermissionEnum.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self view_roles()
+ * @method static self create_role()
+ * @method static self update_role()
+ * @method static self delete_role()
+ * @method static self view_users()
+ * @method static self create_user()
+ * @method static self update_user()
+ * @method static self delete_user()
+ * @method static self view_branches()
+ * @method static self create_branch()
+ * @method static self update_branch()
+ * @method static self delete_branch()
+ * @method static self view_order_sources()
+ * @method static self create_order_source()
+ * @method static self update_order_source()
+ * @method static self delete_order_source()
+ * @method static self view_resellers()
+ * @method static self create_reseller()
+ * @method static self update_reseller()
+ * @method static self delete_reseller()
+ * @method static self view_product_categories()
+ * @method static self create_product_category()
+ * @method static self update_product_category()
+ * @method static self delete_product_category()
+ * @method static self view_products()
+ * @method static self create_product()
+ * @method static self update_product()
+ * @method static self delete_product()
+ * @method static self view_inventories()
+ * @method static self create_inventory()
+ * @method static self create_negative_quantity_inventory()
+ * @method static self view_product_inventories()
+ * @method static self view_orders()
+ * @method static self create_order()
+ * @method static self delete_order()
+ * @method static self view_item_categories()
+ * @method static self create_item_category()
+ * @method static self update_item_category()
+ * @method static self delete_item_category()
+ * @method static self view_items()
+ * @method static self create_item()
+ * @method static self update_item()
+ * @method static self delete_item()
+ * @method static self view_item_inventories()
+ * @method static self view_purchases()
+ * @method static self create_purchase()
+ * @method static self delete_purchase()
+ */
+final class PermissionEnum extends Enum
+{
+}
