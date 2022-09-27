d47da9af0a485f41356fc0d2941ee427de1d2ccc
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-09-27 23:13:04 +0700

    feat(permission): set view permission

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -30,111 +30,160 @@
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>Dashboard</p>
                 </x-nav-item>
-                <li class="nav-header">{{ __('Sales') }}</li>
-                <x-nav-item
-                    :href="url('/orders')"
-                    activeHref="orders"
-                >
-                    <i class="nav-icon fas fa-dollar-sign"></i>
-                    <p>{{ __('Orders') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/inventories')"
-                    activeHref="inventories"
-                >
-                    <i class="nav-icon fas fa-exchange-alt"></i>
-                    <p>{{ __('Inventories') }}</p>
-                </x-nav-item>
-                <li class="nav-header">{{ __('Purchasing') }}</li>
-                <x-nav-item
-                    :href="url('/purchases')"
-                    activeHref="purchases"
-                >
-                    <i class="nav-icon fas fa-dollar-sign"></i>
-                    <p>{{ __('Purchases') }}</p>
-                </x-nav-item>
-                <li class="nav-header">{{ __('Report') }}</li>
-                <x-nav-item
-                    :href="url('/product-inventories')"
-                    activeHref="product-inventories"
-                >
-                    <i class="nav-icon fas fa-box-open"></i>
-                    <p>{{ __('Product Inventories') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/item-inventories')"
-                    activeHref="item-inventories"
-                >
-                    <i class="nav-icon fas fa-box-open"></i>
-                    <p>{{ __('Item Inventories') }}</p>
-                </x-nav-item>
-                <li class="nav-header">{{ __('Product Catalog') }}</li>
-                <x-nav-item
-                    :href="url('/products')"
-                    activeHref="products"
-                >
-                    <i class="nav-icon fas fa-box"></i>
-                    <p>{{ __('Products') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/product-categories')"
-                    activeHref="product-categories"
-                >
-                    <i class="nav-icon fas fa-boxes"></i>
-                    <p>{{ __('Product Categories') }}</p>
-                </x-nav-item>
-                <li class="nav-header">{{ __('Item Catalog') }}</li>
-                <x-nav-item
-                    :href="url('/items')"
-                    activeHref="items"
-                >
-                    <i class="nav-icon fas fa-box"></i>
-                    <p>{{ __('Items') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/item-categories')"
-                    activeHref="item-categories"
-                >
-                    <i class="nav-icon fas fa-boxes"></i>
-                    <p>{{ __('Item Categories') }}</p>
-                </x-nav-item>
-                <li class="nav-header">{{ __('Master') }}</li>
-                <x-nav-item
-                    :href="url('/resellers')"
-                    activeHref="resellers"
-                >
-                    <i class="nav-icon fas fa-user-tag"></i>
-                    <p>{{ __('Resellers') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/order-sources')"
-                    activeHref="order-sources"
-                >
-                    <i class="nav-icon fas fa-truck-loading"></i>
-                    <p>{{ __('Order Sources') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/branches')"
-                    activeHref="branches"
-                >
-                    <i class="nav-icon fas fa-network-wired"></i>
-                    <p>{{ __('Branches') }}</p>
-                </x-nav-item>
-                <li class="nav-header">{{ __('Security') }}</li>
-                <x-nav-item
-                    :href="url('/users')"
-                    activeHref="users"
-                >
-                    <i class="nav-icon fas fa-users"></i>
-                    <p>{{ __('Users') }}</p>
-                </x-nav-item>
-                <x-nav-item
-                    :href="url('/roles')"
-                    activeHref="roles"
-                >
-                    <i class="nav-icon fas fa-lock"></i>
-                    <p>{{ __('Roles') }}</p>
-                </x-nav-item>
+                @canany([\App\Enums\PermissionEnum::view_orders()->value,
+                    \App\Enums\PermissionEnum::view_inventories()->value])
+                    <li class="nav-header">{{ __('Sales') }}</li>
+                @endcanany
+                @can(\App\Enums\PermissionEnum::view_orders()->value)
+                    <x-nav-item
+                        :href="url('/orders')"
+                        activeHref="orders"
+                    >
+                        <i class="nav-icon fas fa-dollar-sign"></i>
+                        <p>{{ __('Orders') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_inventories()->value)
+                    <x-nav-item
+                        :href="url('/inventories')"
+                        activeHref="inventories"
+                    >
+                        <i class="nav-icon fas fa-exchange-alt"></i>
+                        <p>{{ __('Inventories') }}</p>
+                    </x-nav-item>
+                @endcan
+                @canany([\App\Enums\PermissionEnum::view_purchases()->value])
+                    <li class="nav-header">{{ __('Purchasing') }}</li>
+                @endcanany
+                @can(\App\Enums\PermissionEnum::view_purchases()->value)
+                    <x-nav-item
+                        :href="url('/purchases')"
+                        activeHref="purchases"
+                    >
+                        <i class="nav-icon fas fa-dollar-sign"></i>
+                        <p>{{ __('Purchases') }}</p>
+                    </x-nav-item>
+                @endcan
+                @canany([\App\Enums\PermissionEnum::view_product_inventories()->value,
+                    \App\Enums\PermissionEnum::view_item_inventories()->value])
+                    <li class="nav-header">{{ __('Report') }}</li>
+                @endcanany
+                @can(\App\Enums\PermissionEnum::view_product_inventories()->value)
+                    <x-nav-item
+                        :href="url('/product-inventories')"
+                        activeHref="product-inventories"
+                    >
+                        <i class="nav-icon fas fa-box-open"></i>
+                        <p>{{ __('Product Inventories') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_item_inventories()->value)
+                    <x-nav-item
+                        :href="url('/item-inventories')"
+                        activeHref="item-inventories"
+                    >
+                        <i class="nav-icon fas fa-box-open"></i>
+                        <p>{{ __('Item Inventories') }}</p>
+                    </x-nav-item>
+                @endcan
+                @canany([\App\Enums\PermissionEnum::view_products()->value,
+                    \App\Enums\PermissionEnum::view_product_categories()->value])
+                    <li class="nav-header">{{ __('Product Catalog') }}</li>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_products()->value)
+                    <x-nav-item
+                        :href="url('/products')"
+                        activeHref="products"
+                    >
+                        <i class="nav-icon fas fa-box"></i>
+                        <p>{{ __('Products') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_product_categories()->value)
+                    <x-nav-item
+                        :href="url('/product-categories')"
+                        activeHref="product-categories"
+                    >
+                        <i class="nav-icon fas fa-boxes"></i>
+                        <p>{{ __('Product Categories') }}</p>
+                    </x-nav-item>
+                @endcan
+                @canany([\App\Enums\PermissionEnum::view_items()->value,
+                    \App\Enums\PermissionEnum::view_item_categories()->value])
+                    <li class="nav-header">{{ __('Item Catalog') }}</li>
+                @endcanany
+                @can(\App\Enums\PermissionEnum::view_items()->value)
+                    <x-nav-item
+                        :href="url('/items')"
+                        activeHref="items"
+                    >
+                        <i class="nav-icon fas fa-box"></i>
+                        <p>{{ __('Items') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_item_categories()->value)
+                    <x-nav-item
+                        :href="url('/item-categories')"
+                        activeHref="item-categories"
+                    >
+                        <i class="nav-icon fas fa-boxes"></i>
+                        <p>{{ __('Item Categories') }}</p>
+                    </x-nav-item>
+                @endcan
+                @canany([\App\Enums\PermissionEnum::view_branches()->value,
+                    \App\Enums\PermissionEnum::view_order_sources()->value,
+                    \App\Enums\PermissionEnum::view_resellers()->value])
+                    <li class="nav-header">{{ __('Master') }}</li>
+                @endcanany
+                @can(\App\Enums\PermissionEnum::view_resellers()->value)
+                    <x-nav-item
+                        :href="url('/resellers')"
+                        activeHref="resellers"
+                    >
+                        <i class="nav-icon fas fa-user-tag"></i>
+                        <p>{{ __('Resellers') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_order_sources()->value)
+                    <x-nav-item
+                        :href="url('/order-sources')"
+                        activeHref="order-sources"
+                    >
+                        <i class="nav-icon fas fa-truck-loading"></i>
+                        <p>{{ __('Order Sources') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_branches()->value)
+                    <x-nav-item
+                        :href="url('/branches')"
+                        activeHref="branches"
+                    >
+                        <i class="nav-icon fas fa-network-wired"></i>
+                        <p>{{ __('Branches') }}</p>
+                    </x-nav-item>
+                @endcan
+                @canany([\App\Enums\PermissionEnum::view_users()->value,
+                    \App\Enums\PermissionEnum::view_roles()->value])
+                    <li class="nav-header">{{ __('Security') }}</li>
+                @endcanany
+                @can(\App\Enums\PermissionEnum::view_users()->value)
+                    <x-nav-item
+                        :href="url('/users')"
+                        activeHref="users"
+                    >
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>{{ __('Users') }}</p>
+                    </x-nav-item>
+                @endcan
+                @can(\App\Enums\PermissionEnum::view_roles()->value)
+                    <x-nav-item
+                        :href="url('/roles')"
+                        activeHref="roles"
+                    >
+                        <i class="nav-icon fas fa-lock"></i>
+                        <p>{{ __('Roles') }}</p>
+                    </x-nav-item>
+                @endcan
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,14 @@ Route::group([
     Route::view('/', 'welcome');
     Route::view('/dashboard', 'welcome');
 
-    Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
+    Route::get('/users', [\App\Http\Controllers\UserController::class, 'index'])->can(\App\Enums\PermissionEnum::view_users());
     Route::post('/users', [\App\Http\Controllers\UserController::class, 'store']);
     Route::get('/users/create', [\App\Http\Controllers\UserController::class, 'create']);
     Route::get('/users/{user}', [\App\Http\Controllers\UserController::class, 'show']);
     Route::put('/users/{user}', [\App\Http\Controllers\UserController::class, 'update']);
     Route::delete('/users/{user}', [\App\Http\Controllers\UserController::class, 'destroy']);
 
-    Route::get('/roles', [\App\Http\Controllers\RoleController::class, 'index']);
+    Route::get('/roles', [\App\Http\Controllers\RoleController::class, 'index'])->can(\App\Enums\PermissionEnum::view_roles());
     Route::get('/roles/create', [\App\Http\Controllers\RoleController::class, 'create']);
     Route::post('/roles', [\App\Http\Controllers\RoleController::class, 'store']);
     Route::get('/roles/{role}', [\App\Http\Controllers\RoleController::class, 'show']);
@@ -45,54 +45,54 @@ Route::group([
     Route::get('/products/import', [\App\Http\Controllers\ProductImportController::class, 'index']);
     Route::post('/products/import', [\App\Http\Controllers\ProductImportController::class, 'store']);
 
-    Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index']);
+    Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index'])->can(\App\Enums\PermissionEnum::view_products());
     Route::get('/products/create', [\App\Http\Controllers\ProductController::class, 'create']);
     Route::post('/products', [\App\Http\Controllers\ProductController::class, 'store']);
     Route::get('/products/{product}', [\App\Http\Controllers\ProductController::class, 'show']);
     Route::put('/products/{product}', [\App\Http\Controllers\ProductController::class, 'update']);
     Route::delete('/products/{product}', [\App\Http\Controllers\ProductController::class, 'destroy']);
 
-    Route::get('/branches', [\App\Http\Controllers\BranchController::class, 'index']);
+    Route::get('/branches', [\App\Http\Controllers\BranchController::class, 'index'])->can(\App\Enums\PermissionEnum::view_branches());
     Route::get('/branches/create', [\App\Http\Controllers\BranchController::class, 'create']);
     Route::post('/branches', [\App\Http\Controllers\BranchController::class, 'store']);
     Route::get('/branches/{branch}', [\App\Http\Controllers\BranchController::class, 'show']);
     Route::put('/branches/{branch}', [\App\Http\Controllers\BranchController::class, 'update']);
     Route::delete('/branches/{branch}', [\App\Http\Controllers\BranchController::class, 'destroy']);
 
-    Route::get('/product-categories', [\App\Http\Controllers\ProductCategoryController::class, 'index']);
+    Route::get('/product-categories', [\App\Http\Controllers\ProductCategoryController::class, 'index'])->can(\App\Enums\PermissionEnum::view_product_categories());
     Route::get('/product-categories/create', [\App\Http\Controllers\ProductCategoryController::class, 'create']);
     Route::post('/product-categories', [\App\Http\Controllers\ProductCategoryController::class, 'store']);
     Route::get('/product-categories/{productCategory}', [\App\Http\Controllers\ProductCategoryController::class, 'show']);
     Route::put('/product-categories/{productCategory}', [\App\Http\Controllers\ProductCategoryController::class, 'update']);
     Route::delete('/product-categories/{productCategory}', [\App\Http\Controllers\ProductCategoryController::class, 'destroy']);
 
-    Route::get('/order-sources', [\App\Http\Controllers\OrderSourceController::class, 'index']);
+    Route::get('/order-sources', [\App\Http\Controllers\OrderSourceController::class, 'index'])->can(\App\Enums\PermissionEnum::view_order_sources());
     Route::get('/order-sources/create', [\App\Http\Controllers\OrderSourceController::class, 'create']);
     Route::post('/order-sources', [\App\Http\Controllers\OrderSourceController::class, 'store']);
     Route::get('/order-sources/{orderSource}', [\App\Http\Controllers\OrderSourceController::class, 'show']);
     Route::put('/order-sources/{orderSource}', [\App\Http\Controllers\OrderSourceController::class, 'update']);
     Route::delete('/order-sources/{orderSource}', [\App\Http\Controllers\OrderSourceController::class, 'destroy']);
 
-    Route::get('/resellers', [\App\Http\Controllers\ResellerController::class, 'index']);
+    Route::get('/resellers', [\App\Http\Controllers\ResellerController::class, 'index'])->can(\App\Enums\PermissionEnum::view_resellers());
     Route::get('/resellers/create', [\App\Http\Controllers\ResellerController::class, 'create']);
     Route::post('/resellers', [\App\Http\Controllers\ResellerController::class, 'store']);
     Route::get('/resellers/{reseller}', [\App\Http\Controllers\ResellerController::class, 'show']);
     Route::put('/resellers/{reseller}', [\App\Http\Controllers\ResellerController::class, 'update']);
     Route::delete('/resellers/{reseller}', [\App\Http\Controllers\ResellerController::class, 'destroy']);
 
-    Route::get('/inventories', [\App\Http\Controllers\InventoryController::class, 'index']);
+    Route::get('/inventories', [\App\Http\Controllers\InventoryController::class, 'index'])->can(\App\Enums\PermissionEnum::view_inventories());
     Route::get('/inventories/create', [\App\Http\Controllers\InventoryController::class, 'create']);
     Route::post('/inventories', [\App\Http\Controllers\InventoryController::class, 'store']);
 
-    Route::get('/product-inventories', [\App\Http\Controllers\ProductInventoryController::class, 'index']);
+    Route::get('/product-inventories', [\App\Http\Controllers\ProductInventoryController::class, 'index'])->can(\App\Enums\PermissionEnum::view_product_inventories());
 
-    Route::get('/orders', [\App\Http\Controllers\OrderController::class, 'index']);
+    Route::get('/orders', [\App\Http\Controllers\OrderController::class, 'index'])->can(\App\Enums\PermissionEnum::view_orders());
     Route::get('/orders/create', [\App\Http\Controllers\OrderController::class, 'create']);
     Route::post('/orders', [\App\Http\Controllers\OrderController::class, 'store']);
     Route::get('/orders/{order}', [\App\Http\Controllers\OrderController::class, 'show']);
     Route::delete('/orders/{order}', [\App\Http\Controllers\OrderController::class, 'destroy']);
 
-    Route::get('/item-categories', [\App\Http\Controllers\ItemCategoryController::class, 'index']);
+    Route::get('/item-categories', [\App\Http\Controllers\ItemCategoryController::class, 'index'])->can(\App\Enums\PermissionEnum::view_item_categories());
     Route::get('/item-categories/create', [\App\Http\Controllers\ItemCategoryController::class, 'create']);
     Route::post('/item-categories', [\App\Http\Controllers\ItemCategoryController::class, 'store']);
     Route::get('/item-categories/{itemCategory}', [\App\Http\Controllers\ItemCategoryController::class, 'show']);
@@ -102,16 +102,16 @@ Route::group([
     Route::get('/items/import', [\App\Http\Controllers\ItemImportController::class, 'index']);
     Route::post('/items/import', [\App\Http\Controllers\ItemImportController::class, 'store']);
 
-    Route::get('/items', [\App\Http\Controllers\ItemController::class, 'index']);
+    Route::get('/items', [\App\Http\Controllers\ItemController::class, 'index'])->can(\App\Enums\PermissionEnum::view_items());
     Route::get('/items/create', [\App\Http\Controllers\ItemController::class, 'create']);
     Route::post('/items', [\App\Http\Controllers\ItemController::class, 'store']);
     Route::get('/items/{item}', [\App\Http\Controllers\ItemController::class, 'show']);
     Route::put('/items/{item}', [\App\Http\Controllers\ItemController::class, 'update']);
     Route::delete('/items/{item}', [\App\Http\Controllers\ItemController::class, 'destroy']);
 
-    Route::get('/item-inventories', [\App\Http\Controllers\ItemInventoryController::class, 'index']);
+    Route::get('/item-inventories', [\App\Http\Controllers\ItemInventoryController::class, 'index'])->can(\App\Enums\PermissionEnum::view_item_inventories());
 
-    Route::get('/purchases', [\App\Http\Controllers\PurchaseController::class, 'index']);
+    Route::get('/purchases', [\App\Http\Controllers\PurchaseController::class, 'index'])->can(\App\Enums\PermissionEnum::view_purchases());
     Route::get('/purchases/create', [\App\Http\Controllers\PurchaseController::class, 'create']);
     Route::post('/purchases', [\App\Http\Controllers\PurchaseController::class, 'store']);
     Route::get('/purchases/{purchase}', [\App\Http\Controllers\PurchaseController::class, 'show']);
