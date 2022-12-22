e0ff0bfb2522c98407ee7ed9153a8b9c59c78bac
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-12-23 04:59:07 +0700

    fix(branch): delete users even user_ids is null when updating branch

--- app/Actions/UpdateBranchAction.php
+++ app/Actions/UpdateBranchAction.php
@@ -23,11 +23,10 @@ class UpdateBranchAction
         }
 
         $branch->update($data);
-
+        $branch->users()->delete();
         $userIDs = data_get($data, 'user_ids');
 
         if ($userIDs && count($userIDs) > 0) {
-            $branch->users()->delete();
             $branch->users()->createMany(
                 array_map(fn ($id) => ['user_id' => $id], $userIDs)
             );
